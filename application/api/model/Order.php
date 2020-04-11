7db85b7dca47e7fc83011b928c166161e5aa7b6c
2687707641 <2687707641@qq.com>
2020-04-11 17:18:30 +0800

    no message

--- application/api/model/Order.php
+++ application/api/model/Order.php
@@ -7,4 +7,15 @@ namespace app\api\model;
 class Order extends Base
 {
     protected $autoWriteTimestamp = 'datetime';
+
+    //订单状态读取器
+    public function getStatusAttr($value)
+    {
+        $status = [
+            '1' => '待发货',
+            '2' => '已发货',
+            '3' => '确认收货',
+        ];
+        return $status[$value];
+    }
 }
